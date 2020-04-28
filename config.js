5131b09ca0df2c96468d7be347366fd7132a44cf
kvvchetty <kvvchetty@outlook.com>
2020-04-28 08:44:14 -0400

    first commit

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 module.exports = {
-    url: '<Specify Your DB Url here>',
+	url: 'mongodb+srv://mytest101:mytest101@cluster0-3ulwi.gcp.mongodb.net/test?retryWrites=true&w=majority',
     serverport: 3000 
 }
--- server.js
+++ server.js
@@ -1,20 +1,59 @@
 // get dependencies
 const express = require('express');
 const bodyParser = require('body-parser');
-
+const cors = require('cors');
 const app = express();
 
 // parse requests
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-//Enable CORS for all HTTP methods
+/*Enable CORS for all HTTP methods
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE, OPTIONS");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
   });
+*/
+// hard coded configuration object
+conf = {
+    // look for PORT environment variable,
+    // else look for CLI argument,
+    // else use hard coded value for port 8080
+    port: process.env.PORT || process.argv[2] || 8080,
+    // origin undefined handler
+    // see https://github.com/expressjs/cors/issues/71
+    originUndefined: function (req, res, next) {
+        if (!req.headers.origin) {
+            res.json({
+                mess: 'Hi you are visiting the service locally. If this was a CORS the origin header should not be undefined'
+            });
+        } else {
+            next();
+        }
+    },
+ 
+    // Cross Origin Resource Sharing Options
+    cors: {
+        // origin handler
+        origin: function (origin, cb) {
+            // setup a white list
+            let wl = ['https://agfndbsrv101.herokuapp.com'];
+            if (wl.indexOf(origin) != -1) {
+                cb(null, true);
+            } else {
+                cb(new Error('invalid origin: ' + origin), false);
+            }
+        },
+        optionsSuccessStatus: 200
+    }
+};
+ 
+// use origin undefined handler, then cors for all paths
+app.use(conf.originUndefined, cors(conf.cors));
+
+
 
 // Configuring the database
 const config = require('./config.js');
@@ -35,7 +74,7 @@ mongoose.connect(config.url, {
 
 // default route
 app.get('/', (req, res) => {
-    res.json({"message": "Welcome to ZeptoBook Product app"});
+    res.json({"message": "Fdbsrv Product app"});
 });
 
 // listen on port 3000

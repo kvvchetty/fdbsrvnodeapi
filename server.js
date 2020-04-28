f7fdd1c796b570e68c406623296c0facaeaac87d
kvvchetty <kvvchetty@outlook.com>
2020-04-28 09:48:37 -0400

    heroku update

--- server.js
+++ server.js
@@ -3,15 +3,14 @@ const bodyParser = require("body-parser");
 const app = express();
 const cors = require("cors");
 //here is the magic
-app.use(cors());
-/*Enable CORS for all HTTP methods
+//app.use(cors());
+/*Enable CORS for all HTTP methods*/
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE, OPTIONS");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
   });
-*/
 
 // parse requests of content-type - application/json
 app.use(bodyParser.json());
@@ -19,15 +18,11 @@ app.use(bodyParser.json());
 // parse requests of content-type - application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: true }));
 
-
-// Configuring the database
+/*
 const config = require('./config.js');
 const mongoose = require('mongoose');
 require('./product.routes.js')(app);
-
 mongoose.Promise = global.Promise;
-
-// Connecting to the database
 mongoose.connect(config.url, {
     useNewUrlParser: true
 }).then(() => {
@@ -36,6 +31,7 @@ mongoose.connect(config.url, {
     console.log('Could not connect to the database. Exiting now...', err);
     process.exit();
 });
+*/
 
 // default route
 app.get('/', (req, res) => {

7754ad4c7bb2568d1de8901eea4cf89d1288bb94
kvvchetty <kvvchetty@outlook.com>
2020-04-28 09:45:35 -0400

    heroku update

--- server.js
+++ server.js
@@ -3,14 +3,15 @@ const bodyParser = require("body-parser");
 const app = express();
 const cors = require("cors");
 //here is the magic
-//app.use(cors());
-//Enable CORS for all HTTP methods
+app.use(cors());
+/*Enable CORS for all HTTP methods
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE, OPTIONS");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
   });
+*/
 
 // parse requests of content-type - application/json
 app.use(bodyParser.json());
